b6bb2ebaba30a75291c79b148f34cfd7d37c035d
darshan275-f <darshanjomalingpatil@gmail.com>
2025-07-26 22:28:34 +0530

    feat: Add express-rate-limit middleware for request limiting

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import { healthCheck } from "./controllers/healthCheck.controller.js";
 import expressUseragent from 'express-useragent';
+import { rateLimit } from 'express-rate-limit'
+
 const useragent = expressUseragent;
 
 dotenv.config({path:'./env'});
 const app=express();
+
+
 
 
 //middlewares
@@ -15,6 +19,23 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(useragent.express());
+
+
+
+
+//express-rate-limit
+const createRateLimiter=(maxReq,time)=>{
+    return rateLimit({
+        max:maxReq,
+        windowMs:time,
+        message:'Too many request try later',
+        standardHeaders:true,
+        legacyHeaders:false
+    })
+}
+
+app.use(createRateLimiter(100,15*60*1000))
+
 
 
 //routes
