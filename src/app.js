e19f84f07c5d9be24a2cd61a2cfe899022afb3f6
darshan275-f <darshanjomalingpatil@gmail.com>
2025-08-03 20:23:20 +0530

    Rate limiting added

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import { healthCheck } from "./controllers/healthCheck.controller.js";
 import expressUseragent from 'express-useragent';
-import { rateLimit } from 'express-rate-limit'
+import helmet from 'helmet';
+import  { RateLimiterRedis } from "rate-limiter-flexible";
+import Redis from 'ioredis';
+import ErrorHandler from "./middlewares/errorHandler.middleware.js";
+
+
+
 
 const useragent = expressUseragent;
 
@@ -19,26 +25,38 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(useragent.express());
+app.use(helmet());
+
+const redisClient = new Redis();
 
 
+//rate Limit
+const RateLimiter=new RateLimiterRedis({
+  storeClient:redisClient,
+  keyPrefix:"middleware",
+  points:100,
+  duration:1
+})
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100, 
-  standardHeaders: true,
-  legacyHeaders: false,
-  validate: { trustProxy: false },
-});
+app.use((req,res,next)=>{
 
-app.use(limiter);
+    if (req.path === '/api/v1/health/check') return next();
 
-
+  RateLimiter.consume(req.ip).then(()=>next()).catch(()=>{
+    console.log(`Too Many requests!! ${req.ip} is Blocked. Try Later`);
+    res.status(429).json({
+      success:false,
+      message:"Too many Requests!!"
+    })
+  })
+})
 
 
 //routes
 app.use('/api/v1/user',userRouter);
-app.use('/api/v1/health',healthCheck)
+app.use('/api/v1/health',healthCheck);
 
 
+app.use(ErrorHandler);
 
 export default app;
---
+++ src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,9 @@
+
+
+const ErrorHandler=(err,req,res,next)=>{
+    res.status(err.status || 500).json({
+        message: err.message || "Internal server error"
+    })
+}
+
+export default ErrorHandler;
